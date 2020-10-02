f258496d53b883a34634bcf4b2db41523d829056
Wang Zijun <wzj97@hotmail.com>
2020-10-02 18:44:27 +0800

    Updates

---
+++ precache-manifest.906729a8f982b998e7785c08f74bdbe6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f44a29c67415d200a4e4d93ccf61f327",
+    "url": "/pl3281-experiment/index.html"
+  },
+  {
+    "revision": "fbbbb8b70ee214be3b53",
+    "url": "/pl3281-experiment/static/css/main.4ab30d0d.chunk.css"
+  },
+  {
+    "revision": "df0efcaa6159887a828d",
+    "url": "/pl3281-experiment/static/js/2.79e4ee86.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pl3281-experiment/static/js/2.79e4ee86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbbbb8b70ee214be3b53",
+    "url": "/pl3281-experiment/static/js/main.6caae339.chunk.js"
+  },
+  {
+    "revision": "7ad5d0edb14b4be3db4f",
+    "url": "/pl3281-experiment/static/js/runtime-main.e0c47dd0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pl3281-experiment/static/media/logo.5d5d9eef.svg"
+  }
+]);
