f75d23216ebf778f7cc96802d4c0cc34633a9044
Wang Zijun <wzj97@hotmail.com>
2020-10-04 20:50:33 +0800

    Updates

--- precache-manifest.29f741ff37c4cc189ea5b569199475dc.js
+++ precache-manifest.29f741ff37c4cc189ea5b569199475dc.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0c166b597dfb1caa8279791b36e6948f",
+    "revision": "0abfcfce7569afa96273105f778083c5",
     "url": "/pl3281-experiment/index.html"
   },
   {
-    "revision": "b194b43e9869a27fab86",
-    "url": "/pl3281-experiment/static/css/main.03683e2a.chunk.css"
+    "revision": "445754f0837a48dd1c60",
+    "url": "/pl3281-experiment/static/css/main.00c28277.chunk.css"
   },
   {
     "revision": "df0efcaa6159887a828d",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/pl3281-experiment/static/js/2.79e4ee86.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "b194b43e9869a27fab86",
-    "url": "/pl3281-experiment/static/js/main.db1b5d64.chunk.js"
+    "revision": "445754f0837a48dd1c60",
+    "url": "/pl3281-experiment/static/js/main.6bcd36a4.chunk.js"
   },
   {
     "revision": "7ad5d0edb14b4be3db4f",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pl3281-experiment/precache-manifest.2cba8b0e56f00c528444141a8e848688.js"
+  "/pl3281-experiment/precache-manifest.29f741ff37c4cc189ea5b569199475dc.js"
 );
 
 self.addEventListener('message', (event) => {
